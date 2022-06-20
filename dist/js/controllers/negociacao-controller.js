a8b2401274441f97ba1d65ecef0003af87d2abff
euclidespaim <euclidespaim@gmail.com>
2022-06-20 14:47:15 -0300

    Array de negociacoes criado

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,14 +1,18 @@
-import { Negociacao } from "../models/negociacao.js";
+import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
 export class NegociacaoController {
     constructor() {
+        this.negociacoes = new Negociacoes();
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
-        this.limpaFormulario();
+        this.negociacoes.adiciona(negociacao);
+        this.negociacoes.lista().pop();
+        console.log(this.negociacoes.lista());
+        this.limparFormulario();
     }
     criaNegociacao() {
         const exp = /-/g;
@@ -17,7 +21,7 @@ export class NegociacaoController {
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
     }
-    limpaFormulario() {
+    limparFormulario() {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';

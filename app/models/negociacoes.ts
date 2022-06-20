819a02b6eea107be9c328402cea979ed5e93c592
euclidespaim <euclidespaim@gmail.com>
2022-06-20 15:40:21 -0300

    Simplificando o codigo

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
-    private negociacoes: Array<Negociacao> = []; 
+    private negociacoes: Negociacao[] = []; 
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
     
-    lista(): Array<Negociacao> {
-        return [...this.negociacoes];
+    lista(): readonly Negociacao[] {
+        return this.negociacoes
     }
 }
--- servidor-api/server.js
+++ servidor-api/server.js
@@ -2,6 +2,6 @@ var http = require('http')
     ,app = require('./config/express');
 
 http.createServer(app).listen(8080, function() {
-    console.log('Servidor escutando na porta: ' + this.address().port);
+    console.log('Servidor escutando na porta: ' + this.address().port());
 });
 

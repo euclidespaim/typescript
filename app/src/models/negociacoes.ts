7d63b44b2761ffe0fb9ecc83003d89e76eec3f9d
euclidespaim <euclidespaim@gmail.com>
2022-06-28 15:24:13 -0300

    Interface com generics

--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -1,7 +1,9 @@
+import { Comparavel } from '../interfaces/comparavel.js';
 import { Imprimivel } from '../utils/imprimivel.js';
 import { Negociacao } from './negociacao.js';
 
-export class Negociacoes implements Imprimivel{
+export class Negociacoes implements Imprimivel, Comparavel{
+    
     private negociacoes: Negociacao[] = [];
 
     public adiciona(negociacao: Negociacao) {
@@ -14,5 +16,9 @@ export class Negociacoes implements Imprimivel{
 
     public paraTexto(): string {
         return JSON.stringify(this.negociacoes, null, 2)
+    }
+
+    public ehIgual(negociacoes: Negociacoes): boolean {
+        return JSON.stringify(this.negociacoes) === JSON.stringify(negociacoes.lista());
     }
 }
